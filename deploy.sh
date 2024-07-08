dfde904cdc78270406e7439020754232e9c20314
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-08 12:01:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,28 @@
 #! /bin/bash
 # GET CURRENT BRANCH:
-BRANCH_NAME ='${GIT_BRANCH.split/.last()'
-
-<<<<<<< HEAD
-#BRANCH_NAME = $(git rev-parse --abbrev -ref HEAD)
+BRANCH_NAME = env.GIT_BRANCH.split('/').last()
+BRANCH_NAME=$(git rev-parse --abbrev -ref HEAD)
 
 
-if [[ "${BRANCH_NAME}" == "dev" ]]; then
-=======
-if [ "${BRANCH_NAME}" -eq "dev" ]
-then
->>>>>>> bfff44ae7002f6be2642dbfd088010dd3103eacb
+if ["$BRANCH_NAME"="dev"]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    echo "haiiii dev"
 
-	./build.sh
- 	echo "haiiii"
-	DOCKER_REPO="smart24/nginximage-dev"
+    DOCKER_REPO="smart24/myapp-dev"
+    docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
+    docker push $DOCKER_REPO:${BUILD_NUMBER}
+    docker push $DOCKER_REPO:latest
 
-elif [ "${BRANCH_NAME}" -eq "main" ]
-then
-	./build.sh
- 	echo "mainnnnnnn"
-	DOCKER_REPO ="smart24/ngincimage-prod"
-	
+elif ["$BRANCH_NAME"="main"]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    echo "haiiii dev"
+    
+    DOCKER_REPO="smart24/myapp-prod"
+    docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
+    docker push $DOCKER_REPO:${BUILD_NUMBER}
+    docker push $DOCKER_REPO:latest
+
 else echo "Branch not configured for deployment" exit 1
 fi
-  
-docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
-docker push $DOCKER_REPO:${BUILD_NUMBER}
-docker push $DOCKER_REPO:latest
-
-    
-  
-    
-
-  
-
-
-
-
-             
-              
-  
-
-                 
-    
-  
-    
-
-  
-
